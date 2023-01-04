8d48ba860890732c82c1c3143e06eb03b1c17755
NIRAJSHARMA0509 <120631030+NIRAJSHARMA0509@users.noreply.github.com>
2023-01-04 12:47:39 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -2,14 +2,22 @@ import React,{useState,useEffect} from 'react'
 import '../styles/App.css';
 const App = () => {
 //code here
+  const[name,setName]=useState('');
+  const [copyName,setCopyName]=useState(false);
+  const doname=(e)=>{
+    setName(e.target.value);
+  }
+  
+   const click=()=>{
+	setCopyName(true);
+   }
   return (
     <div id="main">
-      <input id='input' onChange={}></input>
-      <button id='button' onClick={}>Click</button>
-      <p id='text'> Hello my name is ____ and I study at Newton School</p>
+      <input type="text"  value={name} onChange={doname}></input>
+      <button id='button' onClick={click}>Click</button>
+	  {copyName ? <p id='text'>Hello my name is {name} and I study at Newton School</p> :
+    <p id='text'>Hello my name is ____ and I study at Newton School</p>
+	}
     </div>
   )
 }
-
-
-export default App;
